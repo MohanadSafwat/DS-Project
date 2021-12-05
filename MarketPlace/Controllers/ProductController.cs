c592a88677e0a601076f7c9cbd619db5a95c16ab
Ahmed Alaa <a99_bdr@yahoo.com>
2021-12-05 23:22:31 +0200

    MVC

---
+++ MarketPlace/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarketPlace.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Product()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-**DS Project**
-
-This is a simple and easy to integrate Marketplace design template based on Bootstrap 5.
